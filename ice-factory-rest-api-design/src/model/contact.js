7bc5b60ae11ac3a58f8f44609ff27cd379b4f7f3
bipinstha7 <bipin.shrestha901@gmail.com>
2019-05-24 00:32:22 +0545

    contact model

---
+++ ice-factory-rest-api-design/src/model/contact.js
@@ -0,0 +1,22 @@
+// import mongoose from 'mongoose'
+const mongoose = require('mongoose')
+
+const contactSchema = new mongoose.Schema({
+	firstName: {
+		type: String,
+		required: true,
+	},
+	lastName: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+})
+
+// const Contact = mongoose.model('Contact', contactSchema)
+// export default Contact
+
+module.exports = mongoose.model('Contact', contactSchema)
