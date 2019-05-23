115a5c9bae6d8ecb88d3765c61778f29fdc8908f
bipinstha7 <bipin.shrestha901@gmail.com>
2019-05-23 21:33:22 +0545

    mongodb connection

--- ice-factory-rest-api-design/src/server.js
+++ ice-factory-rest-api-design/src/server.js
@@ -1,12 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+
 import handleContactRequest from './contacts'
 import adpatRequest from './helpers/adapt-request'
+import { mongoURI } from './config/keys'
 
 const app = express()
 app.use(express.json())
 
+mongoose.connect(mongoURI, { useNewUrlParser: true })
+	.then(() => console.log('MongoDB Connected'))
+	.catch(err => console.log({ Error: err }))
+
 app.all('/contacts', contactsController)
 app.get('contats/:id', contactsController)
+
 
 function contactsController(req, res) {
 	const httpRequest = adpatRequest(req)
