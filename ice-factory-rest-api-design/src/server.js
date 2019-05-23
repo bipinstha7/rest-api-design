4455ff9545a97edd0d217502445d5a1f3485a11e
bipinstha7 <bipin.shrestha901@gmail.com>
2019-05-24 00:40:44 +0545

    use common js importing method instead of es6

--- ice-factory-rest-api-design/src/server.js
+++ ice-factory-rest-api-design/src/server.js
@@ -1,25 +1,32 @@
-import express from 'express'
-import mongoose from 'mongoose'
+// import express from 'express'
+// import mongoose from 'mongoose'
 
-import handleContactRequest from './contacts'
-import adpatRequest from './helpers/adapt-request'
-import { mongoURI } from './config/keys'
+// import handleContactRequest from './contacts'
+// import adpatRequest from './helpers/adapt-request'
+// import { mongoURI } from './config/keys'
+
+const express = require('express')
+const mongoose = require('mongoose')
+
+const handleContactRequest = require('./contacts')
+const adpatRequest = require('./helpers/adapt-request')
+const { mongoURI } = require('./config/keys')
 
 const app = express()
 app.use(express.json())
 
-mongoose.connect(mongoURI, { useNewUrlParser: true })
+mongoose
+	.connect(mongoURI, { useNewUrlParser: true })
 	.then(() => console.log('MongoDB Connected'))
 	.catch(err => console.log({ Error: err }))
 
 app.all('/contacts', contactsController)
 app.get('contats/:id', contactsController)
 
-
 function contactsController(req, res) {
 	const httpRequest = adpatRequest(req)
 	handleContactRequest(httpRequest)
-		.then(({ headers, statusCode, data }) => 
+		.then(({ headers, statusCode, data }) =>
 			res
 				.set(headers)
 				.status(statusCode)
@@ -33,3 +40,5 @@ const port = process.env.PORT || 3000
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`)
 })
+
+export default contactsController
