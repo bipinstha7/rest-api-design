a5a3cd45de07c1748599d15b69157be7d9b1c0f7
bipinstha7 <bipin.shrestha901@gmail.com>
2019-06-13 22:58:57 +0545

    mongoose daatabase connection

---
+++ class-based-rest-api-design/design_1.1.0/src/startup/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const keys = require('../config/keys')
+
+module.exports = () => {
+	mongoose
+		.connect(keys.mongoURI, { useNewUrlParser: true })
+		.then(() => console.log('MongoDB is connected'))
+		.catch(err => console.log({ err }))
+}
