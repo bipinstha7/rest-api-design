fb7285cac9715dfae1c78161fd66c834d069cca4
bipinstha7 <bipin.shrestha901@gmail.com>
2019-06-13 22:58:37 +0545

    Item service which handles the items based crud services

---
+++ class-based-rest-api-design/design_1.1.0/src/services/items.js
@@ -0,0 +1,41 @@
+class ItemsService {
+	constructor(ItemModel) {
+		this.ItemModel = ItemModel
+	}
+
+	async getMyItems(user) {
+		return this.ItemModel.find({ owner: user._id }).populate({ path: 'owner', select: '-password -salt' })
+	}
+
+	async getItem(itemId, user) {
+		return this.ItemModel.findOne({ _id: itemId, owner: user._id }).populate({
+			path: 'owner',
+			select: '-password -salt',
+		})
+	}
+
+	async create(itemDTO, user) {
+		const item = {
+			...itemDTO,
+			owner: user._id,
+		}
+
+		return this.ItemModel.create(item).populate({ path: 'owner', select: '-password -salt' })
+	}
+
+	async update(itemId, itemDTO, user) {
+		const item = {
+			...itemDTO,
+			_id: itemId,
+			owner: user._id,
+		}
+
+		return this.ItemModel.findOneAndUpdate({ _id: itemId, owner: user._id }, item, {
+			new: true,
+		}).populate({ path: 'owner', select: '-password -salt' })
+	}
+
+	async remove(itemId, user) {
+		return this.ItemModel.remove({ _id: itemId, owner: user._id })
+	}
+}
