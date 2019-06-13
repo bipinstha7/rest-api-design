02e7b2f8e7f25f1f6ee8fda86fff26d88c295210
bipinstha7 <bipin.shrestha901@gmail.com>
2019-06-13 22:53:13 +0545

    Users router
    It has 3 routers-
      - /login : to login the user with email and password
      - /login-as : to login the user with just email and only authorize to admin
      - /signup : register user

---
+++ class-based-rest-api-design/design_1.1.0/src/routes/users.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const router = express.Router()
+
+const UserModel = require('../models/user')
+const AuthService = require('../services/auth')
+const isAuth = require('../middlewares/isAuth')
+const attachCurrentUser = require('../middlewares/attachCurrentUser')
+const checkRole = require('../middlewares/checkRole')
+
+router.post('/login', async (req, res) => {
+	const email = req.body.user.email
+	const password = req.body.user.password
+
+	try {
+		const authServiceInstance = new AuthService(UserModel)
+		const { user, token } = await authServiceInstance.login(email, password)
+		res.status(200).json({ user, token })
+	} catch (error) {
+		next(error)
+	}
+})
+
+router.post('/login-as', isAuth, attachCurrentUser, checkRole('admin'), async (req, res) => {
+	try {
+		const email = req.body.user.email
+		const authServiceInstance = new AuthService(UserModel)
+		const { user, token } = await authServiceInstance.loginAs(email)
+		res.status(200).json({ user, token })
+	} catch (error) {
+		next(error)
+	}
+})
+
+router.post('/signup', async (req, res) => {
+	try {
+		const { name, email, password } = req.body.user
+		const authServiceInstance = new AuthService(UserModel)
+
+		const { user, token } = await authServiceInstance.signUp(email, password, name)
+		res.status(200).json({ user, token })
+	} catch (error) {
+		next(error)
+	}
+})
+
+module.exports = router
