2e7137abb41feee5bc7e9824ce4978c030bca60d
bipinstha7 <bipin.shrestha901@gmail.com>
2019-06-13 22:52:47 +0545

    user schema

---
+++ class-based-rest-api-design/design_1.1.0/src/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		lowercase: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	salt: {
+		type: String,
+		required: true,
+	},
+	name: {
+		type: String,
+	},
+	role: {
+		type: String,
+		default: 'user', // Possible values: user || admin
+	},
+})
+
+module.exports = mongoose.model('User', UserSchema)
